1787aadae856c8df5838217b1f95273fbcb5cc4c
WangFei <wangfei@valtech.ai>
2026-08-19 17:01:07 +0800

    feat: init 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '^/v\\d+(?:/|$)': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
